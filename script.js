7ae6ba3ba6342aca15f5f260319d78bddc330d9c
Shivam shukla <76615146+Shivam-Shukla000@users.noreply.github.com>
2024-08-13 04:09:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function operator(op, num1, num2) {
     if (op == "add") {return add(num1, num2)} 
     else if (op == "sub") {return sub(num1, num2)}
     else if (op == "multiply") {return multiply(num1, num2)}
-    else if (op == "devid") {return devid(num1, num2)}
+    else if (op == "divide") {return divide(num1, num2)}
     else {return "syntax error"}
 }
 function displayValue(e) {
